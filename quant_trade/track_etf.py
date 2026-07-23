d6a306b5bb3365106810d2fb539c499792c56803
Eric Khoo <erickhoo1104@gmail.com>
2026-07-23 15:54:48 +0800

    Update track_etf.py

--- quant_trade/track_etf.py
+++ quant_trade/track_etf.py
@@ -567,9 +567,7 @@ def build_scene_1_message(data, date_str, time_ago_str):
         else:
             match_text = "<b>📚 Historical Match</b>\nInsufficient data"
         # 组装单个资产块
-        block = f"""
-━━━━━━━━━━━━
-
+        block = f"""━━━━━━━━━━━━
 <b>📈 {asset_name}</b>
 
 {trend_color} Trend: {trend_score}/100
